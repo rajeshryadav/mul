bbc6a1d7fc284dd384e772d38a59bc5da29a1021
Rahul kumar <rahulkumar96369@gmail.com>
2019-09-10 23:13:38 +0530

    mul

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int a = 5;
+        int b= 5;
+        int c= a*b;
+        System.out.println(c);
     }
 }
